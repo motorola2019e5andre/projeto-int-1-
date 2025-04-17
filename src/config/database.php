bb0f860662f7521c62436ce67ab34936fd748840
motorola2019e5andre <motorola2019e5andre@gmail.com>
2025-04-16 23:39:56 -0300

    Create database.php

---
+++ src/config/database.php
@@ -0,0 +1,12 @@
+<?php
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
